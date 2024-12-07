7ae26952f89898ed367e8106096a2d728c42d4f8
kureta <skureta@gmail.com>
2024-12-07 22:13:17 +0300

    WIP load and start patch

--- src/puredatagd.cpp
+++ src/puredatagd.cpp
@@ -1,26 +1,66 @@
 #include "puredatagd.h"
 #include "PdTypes.hpp"
+#include "z_libpd.h"
 #include <PdBase.hpp>
+#include <godot_cpp/classes/audio_stream_generator_playback.hpp>
 #include <godot_cpp/core/class_db.hpp>
+#include <godot_cpp/variant/utility_functions.hpp>
 
 using namespace godot;
 
 void PureDataGD::_bind_methods() {}
 
 PureDataGD::PureDataGD() {
-  // Initialize any variables here.
-  time_passed = 0.0;
+  UtilityFunctions::print("PureDataGD constructor");
 
   // Initialize the PdBase object.
   pd::PdBase pd;
   pd::Patch patch = pd.openPatch("bin/test.pd", ".");
+
+  if (::libpd_init() == 0) {
+    UtilityFunctions::print("Initialized PureData");
+  } else {
+  }
+
+  if (::libpd_init_audio(1, 2, 44100) == 0) {
+    UtilityFunctions::print("Initialized DSP");
+  } else {
+    return;
+  }
+
+  initialized_ = true;
+
+  handle_ = ::libpd_openfile("test.pd", "./bin");
+  if (handle_ == nullptr) {
+    UtilityFunctions::print("Error opening patch");
+  } else {
+    UtilityFunctions::print("Opened patch");
+  }
+}
+
+PureDataGD::~PureDataGD() {
+  UtilityFunctions::print("PureDataGD destructor");
+  ::libpd_closefile(handle_);
 }
 
 void PureDataGD::_process(double delta) {
-  time_passed += delta;
+  if (is_playing()) {
+    Ref<AudioStreamGeneratorPlayback> p = get_stream_playback();
+    if (p.is_valid()) {
+      int nframes = std::min(p->get_frames_available(), 2048);
 
-  Vector2 new_position = Vector2(10.0 + (10.0 * sin(time_passed * 2.0)),
-                                 10.0 + (10.0 * cos(time_passed * 1.5)));
+      int ticks = nframes / libpd_blocksize();
 
-  set_position(new_position);
+      if (::libpd_process_float(ticks, inbuf_, outbuf_) != 0) {
+        return;
+      }
+
+      for (int i = 0; i < nframes; i++) {
+        auto v = Vector2(outbuf_[i * 2], outbuf_[(i * 2) + 1]);
+        v = v.clamp(Vector2(-1, -1), Vector2(1, 1));
+
+        p->push_frame(v);
+      }
+    }
+  }
 }
--- src/puredatagd.h
+++ src/puredatagd.h
@@ -1,23 +1,27 @@
 #ifndef PUREDATAGD_H
 #define PUREDATAGD_H
 
-#include <godot_cpp/classes/sprite2d.hpp>
+#include <godot_cpp/classes/audio_stream_player.hpp>
 
 namespace godot {
 
-class PureDataGD : public Sprite2D {
+class PureDataGD : public AudioStreamPlayer {
   // NOLINTNEXTLINE(modernize-use-auto)
-  GDCLASS(PureDataGD, Sprite2D)
+  GDCLASS(PureDataGD, AudioStreamPlayer);
+
+  void *handle_{};
 
 private:
-  double time_passed;
+  bool initialized_{};
+  float inbuf_[1];
+  float outbuf_[44100 * 2];
 
 protected:
   static void _bind_methods();
 
 public:
   PureDataGD();
-  ~PureDataGD() override = default;
+  ~PureDataGD();
 
   void _process(double delta) override;
   void _notification(int p_notification) {
@@ -25,7 +29,6 @@ public:
     if (p_notification == NOTIFICATION_EXTENSION_RELOADED) {
       // Handle the hot-reload event
       // Re-initialize variables, reload resources, etc.
-      time_passed = 0.0;
       printf("GDExtension has been reloaded.");
     }
   }
