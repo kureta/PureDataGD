2cba6de564777914ae0e250588fe6f6417b78593
kureta <skureta@gmail.com>
2024-12-08 19:52:35 +0300

    Working patch selector
    Can load patches from resources with file type filter. Also added a
    checkbox to toggle DSP status in PureData.

--- src/puredatagd.cpp
+++ src/puredatagd.cpp
@@ -1,37 +1,109 @@
 #include "puredatagd.h"
 #include <godot_cpp/classes/audio_stream_generator_playback.hpp>
-#include <godot_cpp/core/class_db.hpp>
+#include <godot_cpp/classes/file_access.hpp>
+#include <godot_cpp/classes/global_constants.hpp>
+#include <godot_cpp/classes/ref.hpp>
+#include <godot_cpp/core/error_macros.hpp>
 #include <godot_cpp/variant/utility_functions.hpp>
 
 using namespace godot;
 
-void PureDataGD::_bind_methods() {}
+// TODO: PdBase object refers to a single static PureData instance.
+// Changing patch does not proprly close the previous patch (maybe).
+
+// Define things that will be called by or seen inside the Godot GUI.
+void PureDataGD::_bind_methods() {
+  ClassDB::bind_method(D_METHOD("get_patch_path"), &PureDataGD::get_patch_path);
+  ClassDB::bind_method(D_METHOD("set_patch_path", "p_patch_path"),
+                       &PureDataGD::set_patch_path);
+  ClassDB::bind_method(D_METHOD("get_dsp_on"), &PureDataGD::get_dsp_on);
+  ClassDB::bind_method(D_METHOD("set_dsp_on", "p_dsp_on"),
+                       &PureDataGD::set_dsp_on);
+
+  ADD_PROPERTY(
+      PropertyInfo(Variant::STRING, "patch_path", PROPERTY_HINT_FILE, "*.pd"),
+      "set_patch_path", "get_patch_path");
+
+  // TODO: conntect DSP on to Playing property of AudioStreamPlayer
+  ADD_PROPERTY(PropertyInfo(Variant::BOOL, "DSP on"), "set_dsp_on",
+               "get_dsp_on");
+}
+
+// Convert resource path String to a FileAccess object.
+// NOTE: use `const` if the function does not modify the parameter.
+Ref<FileAccess> resource_path_to_file(const String &path) {
+  Ref<FileAccess> file = FileAccess::open(path, FileAccess::READ);
+
+  return file;
+}
+
+// Check if a resource exists
+bool file_exists(const String &path) {
+  Ref<FileAccess> file = resource_path_to_file(path);
+  if (!file.is_valid())
+    ERR_PRINT(String("File does not exist: ") + path);
+  return file.is_valid();
+}
+
+void PureDataGD::init() {
+  // I guess there is just one global instance of PureData.
+  // We keep track of its initialization state but I don't really understand
+  // this part. I guess that's why the other guy implemented a static singleton.
+  if (!pd.init(1, 2, 44100)) {
+    ERR_PRINT("Failed to initialize PureData!");
+    return;
+  }
+  pd.computeAudio(dsp_on);
+}
+
+void PureDataGD::load_patch() {
+  if (!file_exists(patch_path))
+    return;
+
+  patch = pd.openPatch(
+      resource_path_to_file(patch_path)->get_path_absolute().utf8().get_data(),
+      "/");
+}
 
 PureDataGD::PureDataGD() {
   UtilityFunctions::print("PureDataGD constructor");
-
-  pd.openPatch("test.pd", "./bin");
-
-  if (!initialized) {
-    initialized = pd.init(1, 2, 44100);
-    if (initialized) {
-      UtilityFunctions::print("Initialized PureData.");
-    } else {
-      UtilityFunctions::print("Failed to initialize PureData!");
-    }
-  } else {
-    UtilityFunctions::print(
-        "Constructor called, but PureData was already initialized");
-  }
-
-  pd.computeAudio(true);
+  init();
+  load_patch();
 }
 
+// Closing the patch in the destructor works but if we close or
+// `clear()` the pd object there's no sound in Godot.
 PureDataGD::~PureDataGD() {
   UtilityFunctions::print("PureDataGD destructor");
   pd.closePatch(patch);
+  patch.clear();
+  pd.computeAudio(false);
+  pd.clear();
 }
 
+String PureDataGD::get_patch_path() { return patch_path; }
+
+void PureDataGD::set_patch_path(const String path) {
+  UtilityFunctions::print("Trying to set path to: ", path);
+  patch_path = path;
+  if (!file_exists(patch_path))
+    return;
+
+  pd.closePatch(patch);
+  patch.clear();
+  load_patch();
+
+  UtilityFunctions::print("Set patch path to: ", patch_path);
+}
+
+bool PureDataGD::get_dsp_on() { return dsp_on; }
+void PureDataGD::set_dsp_on(bool status) {
+  pd.computeAudio(status);
+  dsp_on = status;
+}
+
+// This will suffice for a long time. Just runs PD and fills Godot's audio
+// buffer.
 void PureDataGD::_process(double delta) {
   if (is_playing()) {
     Ref<AudioStreamGeneratorPlayback> p = get_stream_playback();
--- src/puredatagd.h
+++ src/puredatagd.h
@@ -3,7 +3,9 @@
 
 #include <PdBase.hpp>
 #include <PdTypes.hpp>
+#include <gdextension_interface.h>
 #include <godot_cpp/classes/audio_stream_player.hpp>
+#include <godot_cpp/classes/file_access.hpp>
 
 namespace godot {
 
@@ -16,7 +18,10 @@ private:
   std::array<float, 44100 * 2> outbuf_;
   pd::PdBase pd{};
   pd::Patch patch{};
-  bool initialized = false;
+  bool dsp_on = false;
+  String patch_path{};
+  void init();
+  void load_patch();
 
 protected:
   static void _bind_methods();
@@ -26,6 +31,10 @@ public:
   ~PureDataGD() override;
 
   void _process(double delta) override;
+  void set_patch_path(const String path);
+  String get_patch_path();
+  void set_dsp_on(const bool status);
+  bool get_dsp_on();
 };
 
 } // namespace godot
