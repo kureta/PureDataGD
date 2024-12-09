370486b600b3f08e6d9cbc711dd6e099d08af796
kureta <skureta@gmail.com>
2024-12-10 01:40:12 +0300

    Use node properties from GUI
    Patch Path selection etc. inside GUI effects game.

--- demo/main.gd
+++ demo/main.gd
@@ -1,17 +1,15 @@
 extends Node
 
 var t = 0.0
+var f = 0.0
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
 	$PureDataGD.play()
 	$PureDataGD.dsp_on = true
-	
-	var p = ProjectSettings.globalize_path("res://patches/test-2.pd")
-	$PureDataGD.patch_path = p
-	$PureDataGD.freq = 440.0
+	f = $PureDataGD.freq
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta):
 	t += delta
-	$PureDataGD.freq = $Sprite2D.position.y + 440
+	$PureDataGD.freq = $Sprite2D.position.y + f - 256
