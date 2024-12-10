db93c116c2521497e03162c55d69565846dc00ab
kureta <skureta@gmail.com>
2024-12-10 20:47:04 +0300

    Works also when exported
    I couldn't load bytes directly in libpd. I needed a file path. Otherwise
    exported game breaks. So I added some GDScript code that loads the
    contents of the pd file into a buffer, writes it into a tmp.pd file and
    passes its path to libpd.

--- demo/main.gd
+++ demo/main.gd
@@ -9,6 +9,15 @@ func _ready():
 	$PureDataGD.dsp_on = true
 	f = $PureDataGD.freq
 
+	var patch_file = FileAccess.open($PureDataGD.patch_path, FileAccess.READ)
+	var tmp_file = FileAccess.open('/tmp/{0}.pd'.format([randi()]), FileAccess.WRITE)
+	var contents = patch_file.get_buffer(patch_file.get_length())
+	patch_file.close()
+	tmp_file.store_buffer(contents)
+	tmp_file.close()
+	
+	$PureDataGD.patch_path = tmp_file.get_path_absolute()
+
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta):
 	t += delta
